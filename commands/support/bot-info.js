2e0e759cf66ed7460a9e03856c17ec6aadcc504c
Christopher <firedoglo1941@gmail.com>
2019-03-04 10:10:35 -0500

    Add files via upload

---
+++ commands/support/bot-info.js
@@ -0,0 +1,43 @@
+const Commando = Depends.Commando
+const Discord = Depends.Discord
+
+class BotCommand extends Commando.Command { 
+	constructor(client){
+		super(client, {
+			name: 'info',
+			aliases: ['botinfo', 'binfo', 'lyaboo'],
+			group: 'support',
+			memberName: "info",
+			description: 'Will return the Bot Information to User.'
+		});
+	}	
+	
+	async run(message, args){
+        if (message.author.bot) return;
+		if (message.channel.type === "dm") return;
+		if (Settings.Testing === true) return;
+		
+        let FirstEmbed = new Discord.RichEmbed()
+        .setAuthor("Lyaboo", "https://cdn.discordapp.com/avatars/513448452987027478/f63754a4f5e489cf7c03a0d797ca70b3.png?size=256")
+        .setColor("6e00ff")
+	    .setThumbnail("https://cdn.discordapp.com/avatars/513448452987027478/f63754a4f5e489cf7c03a0d797ca70b3.png?size=256")
+        .setFooter("Bot Developer: Pokidat#1254", "https://cdn.discordapp.com/avatars/417835827700301836/808cb0b5521609d1f65d9938a6c790cb.png?size=256")
+        .addField("Bot Library", "Discord.Js")
+		.addField("Bot Prefix", `'${Settings.Prefix}'`)
+	    .addField("Bot Commands", `To view Bot Commands, Please Say '${Settings.Prefix}help'`)
+	    .addField("Bot Version", `${Settings.Version}`);
+		
+		let SecondEmbed = new Discord.RichEmbed()
+		.setAuthor("Lyaboo Dedications", "https://cdn.discordapp.com/avatars/513448452987027478/f63754a4f5e489cf7c03a0d797ca70b3.png?size=256")
+		.setTimestamp()
+		.addField("Clickbait", "Owned by Forgetful#6666")
+		.setFooter("Want to see your Discord Added? DM Pokidat#1254", "https://cdn.discordapp.com/avatars/417835827700301836/808cb0b5521609d1f65d9938a6c790cb.png?size=256");    
+	   
+		message.author.send(FirstEmbed); 
+		message.author.send(SecondEmbed);
+		message.channel.send(":white_check_mark: Please Check Direct Messages for More Information. If you didn't receive any messages, it's because your DM's are disabled.")
+		return;
+	}
+}
+
+module.exports = BotCommand
---
+++ commands/support/user-info.js
@@ -0,0 +1,35 @@
+const Commando = Depends.Commando
+const Discord = Depends.Discord
+
+class UserInfoCommand extends Commando.Command {
+	constructor(client) {
+		super(client, {
+			name: 'user-info',
+			aliases: ['user', 'uinfo', 'whois'],
+			group: 'support',
+			memberName: 'user-info',
+			description: 'Gets information about a user.'
+		});
+	}
+	async run(message, args){
+        if (message.author.bot) return;
+		if (message.channel.type === "dm") return;
+		if (Settings.Testing === true) return;
+		
+		if (!message.author.bot) {
+			message.channel.startTyping();
+			const RichEmbed = new Discord.RichEmbed()
+			.setThumbnail(user.avatarURL)
+			.setDescription(`Info on **${user.tag}** (ID: ${user.id})`)
+			.setColor('0x0000FF')
+			.setTitle(user.tag)
+			.addField('**Guild-based Info:**', `Nickname: ${member.nickname ? member.nickname : 'No nickname'}\nRoles: ${member.roles.map(roles => `\`${roles.name}\``).join(', ')}\nJoined at: ${member.joinedAt}`)
+			.addField('**User Info:**', `Created at: ${user.createdAt}\n${user.bot ? 'Account Type: Bot' : 'Account Type: User'}\nStatus: ${user.presence.status}\nGame: ${user.presence.game ? user.presence.game.name : 'None'}`)
+			.setFooter(`Powered by ${this.client.user.username}`);
+			message.channel.send(RichEmbed);
+			message.channel.stopTyping();
+		} 
+    });
+}
+		
+module.exports = UserInfoCommand
