748fa0129cd9fd6056e8390ea3a01ec4865e0dba
Christopher <firedoglo1941@gmail.com>
2019-03-05 19:28:54 -0500

    Add files via upload

--- commands/music/play.js
+++ commands/music/play.js
@@ -68,8 +68,8 @@ async function Play(Guild, Song) {
 		.on('end', Reason => {
 			if (Reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
 			else console.log(Reason);
-			if (Reason == "Stop command has been used!") Play(Guild, Queue.Queue[0]);
-			if (!Reason == "Stop command has been used!"){
+			if (Reason === "Stop command has been used!") Play(Guild, Queue.Queue[0]);
+			if (Reason !== "Stop command has been used!"){
 				if (Queue.Queue[0].repeat === true) {
 					Play(Guild, Queue.Queue[0]);
 				} else {
