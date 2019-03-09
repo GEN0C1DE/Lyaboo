cc155b5868a6580fe74bc76e98f657344854dc0f
Christopher <firedoglo1941@gmail.com>
2019-03-09 17:05:40 -0500

    Add files via upload

--- commands/support/user-info.js
+++ commands/support/user-info.js
@@ -8,28 +8,47 @@ class UserInfoCommand extends Commando.Command {
 			aliases: ['user', 'uinfo', 'whois'],
 			group: 'support',
 			memberName: 'user-info',
-			description: 'Returns information about a user.'
+			description: 'Gets information about a user.'
 		});
 	}
 	async run(message, args){
-        if (message.author.bot) return;
+        	if (message.author.bot) return;
 		if (message.channel.type === "dm") return;
 		if (Settings.Testing === true) return;
 		
+		const member = message.mentions.members.first()
+
 		if (!message.author.bot) {
 			message.channel.startTyping();
 			const RichEmbed = new Discord.RichEmbed()
-			.setThumbnail(user.avatarURL)
-			.setDescription(`Info on **${user.tag}** (ID: ${user.id})`)
+			.setThumbnail(member.user.avatarURL)
+			.setDescription(`Info on **${member.user.tag}** (ID: ${member.user.id})`)
 			.setColor('0x0000FF')
-			.setTitle(user.tag)
+			.setTitle(member.user.tag)
 			.addField('**Guild-based Info:**', `Nickname: ${member.nickname ? member.nickname : 'No nickname'}\nRoles: ${member.roles.map(roles => `\`${roles.name}\``).join(', ')}\nJoined at: ${member.joinedAt}`)
-			.addField('**User Info:**', `Created at: ${user.createdAt}\n${user.bot ? 'Account Type: Bot' : 'Account Type: User'}\nStatus: ${user.presence.status}\nGame: ${user.presence.game ? user.presence.game.name : 'None'}`)
-			.setFooter(`Powered by ${this.client.user.username}`);
+			.addField('**User Info:**', `Created at: ${(member.user.createdAt)}\n${(member.user.bot ? 'Account Type: Bot' : 'Account Type: User')}\nStatus: ${(member.user.presence.status)}`)
+			.setFooter(`Powered by Lyaboo`);
 			message.channel.send(RichEmbed);
 			message.channel.stopTyping();
-		} 
-    };
+		} else {
+			message.channel.startTyping();
+			request.get(`https://discordbots.org/api/bots/${member.user.id}`, (Error, Results, Body) => {
+				if (Error) return console.error;
+				let Information = JSON.parse(Body);
+				
+				const Embed = new RichEmbed()
+				.setThumbnail(member.user.avatarURL)
+				.setDescription(`Info on **${member.user.tag}** (ID: \`${user.id}\`)`)
+				.setColor('0x0000FF')
+				.setTitle(user.tag)
+				.addField('**Guild-based Info:**', `Nickname: ${member.nickname ? member.nickname : 'No nickname'}\nRoles: ${member.roles.map(roles => `\`${roles.name}\``).join(', ')}\nJoined at: ${member.joinedAt}`)
+				.addField('**User Info:**', `Created at: ${member.user.createdAt}\n${member.user.bot ? 'Account Type: Bot' : 'Account Type: User'}\nStatus: ${member.user.presence.status}\nGame: ${member.user.presence.game ? member.user.presence.game.name : 'None'}`)
+				.addField('**Bot Info:**', `Servers: ${Information.server_count ? `${Information.server_count}` : 'Could not get server count'} \nUpvotes: ${Information.points ? `${Information.points}` : 'Could not get bot stats'} \nDescription: ${Information.shortdesc ? `${Information.shortdesc}` : 'Could not get bot info'}`)
+				.setFooter('Powered by Lyaboo and discordbots.org');
+			message.channel.send(Embed);
+			message.channel.stopTyping();
+		}
+    }
 }
 		
 module.exports = UserInfoCommand
