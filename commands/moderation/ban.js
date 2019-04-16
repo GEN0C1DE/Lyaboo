9b47c487bb7a105c75307f11c384b68fe5ce05b6
Christopher <firedoglo1941@gmail.com>
2019-04-15 20:40:20 -0400

    Add files via upload

---
+++ commands/moderation/ban.js
@@ -0,0 +1,58 @@
+const Commando = Depends.Commando
+const Discord = Depends.Discord
+const Mongoose = Depends.Mongoose
+
+class BanCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'ban',
+			aliases: ['b', 'banish', 'hang', 'sentence', 'hell'],
+            group: 'moderation',
+            memberName: "ban",
+            description: 'Will ban a user for [REASON].'
+        });
+    }
+
+    async run(message, args) {
+        const Args = message.content.split(" ")
+        if (message.author.equals(Settings.Bot.user)) return;
+        if (message.channel.type === "dm") return;
+        if (Settings.Testing === true) return;
+
+        let BannedUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(Args[0]));
+		if(!BannedUser) return message.channel.send(":warning: Couldn't find user.");
+		
+		let Reason = Args.join(" ").slice(22);
+		if (!Reason) Reason = "No Reason Provided!"
+		
+		Settings.Schemas.Mods.findOne({
+			ServerID: message.guild.id
+		}, (Error, Results) => { 
+			if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":warning: You do not have permissions to do that.");
+			if(BannedUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":warning: This person can't be kicked.");
+			message.guild.member(BannedUser).ban(Reason);
+			
+			if(Results){
+				if(Results.Logging === true){
+					if(message.guild.channels.get(Results.LogsChannel)){
+						let LoggingChannel = message.guild.channels.get(Results.LogsChannel);
+						let RichEmbed = new Discord.RichEmbed()
+						.setDescription("Member Banned!")
+						.setColor("#000000")
+						.addField("Banned User", `${BannedUser} with ID ${BannedUser.id}`)
+						.addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+						.addField("Banned In", message.channel)
+						.addField("Time", message.createdAt)
+						.addField("Reason", Reason)
+						.setFooter(`Brought to you by Lyaboo`);
+						
+						LoggingChannel.send(RichEmbed);
+					}
+				}
+			}
+		
+		})
+    }
+}
+
+module.exports = BanCommand
---
+++ commands/moderation/kick.js
@@ -0,0 +1,58 @@
+const Commando = Depends.Commando
+const Discord = Depends.Discord
+const Mongoose = Depends.Mongoose
+
+class KickCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'kick',
+			aliases: ['k', 'kill', 'kamikaze'],
+            group: 'moderation',
+            memberName: "kick",
+            description: 'Will kick a user for [REASON].'
+        });
+    }
+
+    async run(message, args) {
+        const Args = message.content.split(" ")
+        if (message.author.equals(Settings.Bot.user)) return;
+        if (message.channel.type === "dm") return;
+        if (Settings.Testing === true) return;
+
+        let KickedUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(Args[0]));
+		if(!KickedUser) return message.channel.send(":warning: Couldn't find user.");
+		
+		let Reason = Args.join(" ").slice(22);
+		if (!Reason) Reason = "No Reason Provided!"
+		
+		Settings.Schemas.Mods.findOne({
+			ServerID: message.guild.id
+		}, (Error, Results) => { 
+			if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":warning: You do not have permissions to do that.");
+			if(KickedUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":warning: This person can't be kicked.");
+			message.guild.member(KickedUser).kick(Reason);
+			
+			if(Results){
+				if(Results.Logging === true){
+					if(message.guild.channels.get(Results.LogsChannel)){
+						let LoggingChannel = message.guild.channels.get(Results.LogsChannel);
+						let RichEmbed = new Discord.RichEmbed()
+						.setDescription("Member Kicked!")
+						.setColor("#000000")
+						.addField("Kicked User", `${KickedUser} with ID ${KickedUser.id}`)
+						.addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+						.addField("Kicked In", message.channel)
+						.addField("Time", message.createdAt)
+						.addField("Reason", Reason)
+						.setFooter(`Brought to you by Lyaboo`);
+						
+						LoggingChannel.send(RichEmbed);
+					}
+				}
+			}
+		
+		})
+    }
+}
+
+module.exports = KickCommand
