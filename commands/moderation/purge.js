e6917a6143d453f82c86005feda76f6ebe537f46
Christopher <firedoglo1941@gmail.com>
2019-04-19 16:12:41 -0400

    Add files via upload

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -19,19 +19,19 @@ class PurgeCommand extends Commando.Command {
         if (message.channel.type === "dm") return;
         if (Settings.Testing === true) return;
 
-        		
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":warning: You do not have permissions to do that.");
+			
+		const DeleteCount = parseInt(args[0], 10);
+		if(!DeleteCount || DeleteCount < 2 || DeleteCount > 100)
+		return message.channel.send("Please provide a number between 2 and 100 for the number of messages to delete.");
+		
+		const Fetched = await message.channel.fetchMessages({limit: DeleteCount});
+		message.channel.bulkDelete(Fetched)
+			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+			
 		Settings.Schemas.Mods.findOne({
 			ServerID: message.guild.id
 		}, (Error, Results) => { 
-			if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":warning: You do not have permissions to do that.");
-			
-			const DeleteCount = parseInt(args[0], 10);
-			if(!DeleteCount || DeleteCount < 2 || DeleteCount > 100)
-			return message.channel.send("Please provide a number between 2 and 100 for the number of messages to delete.");
-			
-			const Fetched = await message.channel.fetchMessages({limit: DeleteCount});
-			message.channel.bulkDelete(Fetched)
-			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 	  
 			if(Results){
 				if(Results.Logging === true){
